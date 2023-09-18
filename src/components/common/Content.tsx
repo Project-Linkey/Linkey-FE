4cec7a635690a741c436f3e92a637538db23b161
오은희 <72530436+eundol0519@users.noreply.github.com>
2023-09-18 15:06:08 +0900

    Design : Content css
    - PhoneLayout 세팅 및 children css 수정

--- src/components/common/Content.tsx
+++ src/components/common/Content.tsx
@@ -1,17 +1,39 @@
 import React, { ReactNode } from "react";
 
 import { ReactComponent as PhoneLayout } from "../../assets/layout/phoneLayout.svg";
+import styled from "@emotion/styled/macro";
 
 interface ContentType {
   children: ReactNode;
 }
 
-const Content: React.FC<ContentType> = () => {
+const Wrap = styled.div`
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+
+  & svg,
+  .content {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .content {
+    background: white;
+    width: 365px;
+    height: 793px;
+    border-radius: 35px;
+  }
+`;
+
+const Content: React.FC<ContentType> = ({ children }) => {
   return (
-    <div className="phoneLayout">
-      sdsd
+    <Wrap>
       <PhoneLayout />
-    </div>
+      <div className="content">{children}</div>
+    </Wrap>
   );
 };
 
