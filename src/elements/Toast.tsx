828ca2337fa02972d4c113d56b2342368cf85ced
오은희 <72530436+eundol0519@users.noreply.github.com>
2023-09-25 16:44:07 +0900

    Feat : 토스트 컴포넌트 구현

---
+++ src/elements/Toast.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+interface Props {
+  text: string;
+  type: string;
+}
+
+const Toast = ({ text, type }: Props) => {
+  const notify = () => {
+    switch (type) {
+      case "success":
+        toast.success(text, {
+          position: "top-center",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+        break;
+      case "warning":
+        toast.warning(text, {
+          position: "top-center",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+        break;
+      case "error":
+        toast.error(text, {
+          position: "top-center",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+        break;
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={notify}>Notify!</button>
+      <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </div>
+  );
+};
+
+export default Toast;
